ca59c6f8b6b45afb5fe08d43a12563ad07750152
Aaron Thompson <aaronseanthompson@icloud.com>
2022-04-13 20:30:03 +0100

    This adds further JS. Now the rows swap when reaching the end of the row. User does not need to click on 1-0 to start types their word

--- scriptsheet.js
+++ scriptsheet.js
@@ -4,7 +4,44 @@ console.log("Hello, World!")
 var str = "hello";
 var strArr = str.split('', 5);
 var row = 1;
+var placer = 1;
+var keyPress = false; //Needed to stop the user input from going back to 1-0
 var userGuess = []
+
+//----Once the body loads the input goes straight to 1-0 without clicking there------
+function myFunction() {
+    if (keyPress != true) {
+        
+        document.getElementById("1-0").focus();
+        console.log(row + "-" + placer);
+        keyPress = true;
+}}
+
+//---------------------- This function jumps from one input to the next---
+/*
+function jumper(field, placement){
+    if(field.value.length >= field.maxLength){
+        document.getElementById(placement).focus();
+        
+    }
+}
+*/
+//-------------------------------------------------------------------------
+
+function jumper(field){
+    if(field.value.length >= field.maxLength){
+
+        if(placer == 5){
+            row +=1;
+            placer = 0;
+        }
+        document.getElementById(row + "-" + placer).focus();
+        placer += 1;
+        console.log(placer);
+        
+    }
+}
+
 
 
 function submitFunc() {
@@ -36,6 +73,10 @@ function submitFunc() {
     userGuess = [];
     console.log(userGuess);
     console.log(row);
+
+    if (row === 5) {
+        document.getElementById("notification").innerHTML = "You lose!";
+    }
     
 
 }
